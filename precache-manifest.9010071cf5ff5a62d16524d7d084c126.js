d1b948d8b571a4eedd85439b81bbe8a206c29c1d
JACK <zhenya271@gmail.com>
2020-04-10 23:59:29 +0300

    Updates

---
+++ precache-manifest.9010071cf5ff5a62d16524d7d084c126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987ea4d38aad91b8a19304c934c9401d",
+    "url": "/PokemonApp/index.html"
+  },
+  {
+    "revision": "5eb08f8c62c4831993dc",
+    "url": "/PokemonApp/static/css/main.719ad958.chunk.css"
+  },
+  {
+    "revision": "8d723873bf3bbffe31ff",
+    "url": "/PokemonApp/static/js/2.e7de84b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokemonApp/static/js/2.e7de84b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eb08f8c62c4831993dc",
+    "url": "/PokemonApp/static/js/main.effce0dc.chunk.js"
+  },
+  {
+    "revision": "ea543c7a5ea4d1403cfc",
+    "url": "/PokemonApp/static/js/runtime-main.1ad23dc9.js"
+  }
+]);
